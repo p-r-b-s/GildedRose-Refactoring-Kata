613d3fe6b483fcf066f11d6c20257ab3f71f4ef1
B S <pseudo.random.ben.s@gmail.com>
2020-08-04 19:09:38 +0000

    Refactored gilded_rose.go

--- go/gilded-rose.go
+++ go/gilded-rose.go
@@ -1,5 +1,8 @@
 package main
 
+import (
+  "strings"
+)
 
 type Item struct {
 	name            string
@@ -7,55 +10,45 @@ type Item struct {
 }
 
 func UpdateQuality(items []*Item) {
+	for _, item := range items {
+    switch {
+      case strings.HasPrefix(item.name, "Aged Brie"):
+        update_brie(item)
+      case strings.HasPrefix(item.name, "Sulfuras"):
+      //noop
+      case strings.HasPrefix(item.name, "Backstage passes"):
+        update_passes(item)
+      default:
+        update_item(item)
+    }
+  }
+}		
 
-	for i := 0; i < len(items); i++ {
-
-
-		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-			if items[i].quality > 0 {
-				if items[i].name != "Sulfuras, Hand of Ragnaros" {
-					items[i].quality = items[i].quality - 1
-				}
-			}
-		} else {
-			if items[i].quality < 50 {
-				items[i].quality = items[i].quality + 1
-				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].sellIn < 11 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-					if items[i].sellIn < 6 {
-						if items[i].quality < 50 {
-							items[i].quality = items[i].quality + 1
-						}
-					}
-				}
-			}
-		}
-
-		if items[i].name != "Sulfuras, Hand of Ragnaros" {
-			items[i].sellIn = items[i].sellIn - 1
-		}
-
-		if items[i].sellIn < 0 {
-			if items[i].name != "Aged Brie" {
-				if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].quality > 0 {
-						if items[i].name != "Sulfuras, Hand of Ragnaros" {
-							items[i].quality = items[i].quality - 1
-						}
-					}
-				} else {
-					items[i].quality = items[i].quality - items[i].quality
-				}
-			} else {
-				if items[i].quality < 50 {
-					items[i].quality = items[i].quality + 1
-				}
-			}
-		}
+func update_brie (item *Item) {
+  item.sellIn = item.sellIn -1
+  if item.quality < 50 {
+		item.quality = item.quality + 1
 	}
+}
 
+func update_passes (item *Item) {
+  item.sellIn = item.sellIn -1
+  switch {
+    case item.sellIn > 10:
+		  item.quality = item.quality + 1
+    case item.sellIn > 5:
+	  	item.quality = item.quality + 2
+    case item.sellIn > 0:
+	  	item.quality = item.quality + 3
+    default:
+      item.quality = 0
+  }
+  if item.quality > 50 {
+		item.quality = 50
+	}
+}
+
+func update_item (item *Item) {
+  item.sellIn = item.sellIn -1
+  item.quality = item.quality - 1
 }
