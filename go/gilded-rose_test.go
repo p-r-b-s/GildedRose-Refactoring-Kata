46df101579997386600b1510d81dcb5477eabfb3
B S <pseudo.random.ben.s@gmail.com>
2020-08-05 10:26:22 +0000

    created test

--- go/gilded-rose_test.go
+++ go/gilded-rose_test.go
@@ -2,14 +2,22 @@ package main
 
 import "testing"
 
-func Test_Foo(t *testing.T) {
+func Test_Conjured(t *testing.T) {
 	var items = []*Item{
-		&Item{"foo", 0, 0},
+		&Item{"Conjured fluff", 1, 2},
 	}
 
 	UpdateQuality(items)
 
-	if items[0].name != "fixme" {
+	if items[0].name != "Conjured fluff" {
 		t.Errorf("Name: Expected %s but got %s ", "fixme", items[0].name)
 	}
+  if items[0].sellIn != 0 {
+		t.Errorf("sellIn: Expected %d but got %d ", 0, items[0].sellIn)
+	}
+  if items[0].quality != 0 {
+		t.Errorf("quality: Expected %d but got %d ", 0, items[0].quality)
+	}
 }
+
+
