88b685e62db55827e676570392379abc4628f83d
B S <pseudo.random.ben.s@gmail.com>
2020-08-07 10:13:40 +0000

    part python soln

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -4,11 +4,49 @@ import unittest
 from gilded_rose import Item, GildedRose
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+
+  backstage_pass_array_quality = [(30,1)]
+  #You are here. We're looking at a tuple that has the SellIn and Q values
+
+    def test_update_normal_item(self):
+        items = [Item("foo", 1, 1)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEquals("foo", items[0].name)
+        self.assertEquals(0, items[0].sell_in)
+        self.assertEquals(0, items[0].quality)
+
+    def test_quality_never_negative(self):
+        items = [Item("foo", 1, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals("foo", items[0].name)
+        self.assertEquals(0, items[0].sell_in)
+        self.assertEquals(0, items[0].quality)
+
+    def test_aged_brie_increases_in_quality(self):
+        items = [Item("Aged Brie", 1, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals("Aged Brie", items[0].name)
+        self.assertEquals(0, items[0].sell_in)
+        self.assertEquals(1, items[0].quality)
+
+    def test_quality_max_limit(self):
+        items = [Item("Aged Brie", 1, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEquals("Aged Brie", items[0].name)
+        self.assertEquals(0, items[0].sell_in)
+        self.assertEquals(50, items[0].quality)
+
+    # def test_quality_max_limit(self):
+    #     items = [Item("foo", 1, 52)]
+    #     gilded_rose = GildedRose(items)
+    #     gilded_rose.update_quality()
+    #     self.assertEquals("foo", items[0].name)
+    #     self.assertEquals(0, items[0].sell_in)
+    #     self.assertEquals(50, items[0].quality)
 
 if __name__ == '__main__':
     unittest.main()
